16102469b0c798d4ee5a5c3ab772ade821df6eb0
lchdevjinzy <345254523@qq.com>
2020-04-15 17:34:24 +0800

    新的改动

--- demo11/src/main/java/com/example/demo11/controller/HelloController.java
+++ demo11/src/main/java/com/example/demo11/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.demo11.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo11.bean.User;
 import com.example.demo11.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -28,19 +30,19 @@ public class HelloController {
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(String name) {
+    @ResponseBody
+    public JSONObject login(String name) {
 
-        System.out.println(name);
+        JSONObject res= new JSONObject();
 
         User userBean = userService.getUserPo(name);
-
-        System.out.println("userBean:"+userBean);
-
         if (userBean != null) {
-            return "success";
+            res.put("message","success");
         } else {
-            return "login";
+            res.put("message","fail");
         }
+        System.out.println(res.toString());
 
+        return res;
     }
-}-
+}
